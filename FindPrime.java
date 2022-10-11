ea672d31a4894e36326038beb7e30557e563a531
madhaviboga <113083121+madhaviboga@users.noreply.github.com>
2022-10-11 06:04:40 +0530

    Create FindPrime.java

---
+++ FindPrime.java
@@ -0,0 +1,21 @@
+public class FindPrime {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int num;
+				 boolean isPrime;
+				 num = 14;
+				 if(num < 2) isPrime = false;
+				 else isPrime = true;
+				 for(int i=2; i <= num/i; i++) {
+				 if((num % i) == 0) {
+				 isPrime = false;
+				 break;
+				 }
+				 }
+				 if(isPrime) System.out.println("Prime");
+				 else System.out.println("Not a Prime");
+
+			}
+
+		}
