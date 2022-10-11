52a978113ac08cfc854d31083df32d85bec33d86
madhaviboga <113083121+madhaviboga@users.noreply.github.com>
2022-10-11 06:03:40 +0530

    Create DoWhile.java

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+public class DoWhile {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int n = 10;
+				 do {
+				 System.out.println("tick " + n);
+				 n--;
+				 } while(n > 0);
+				
+			}
+
+		}
+		
