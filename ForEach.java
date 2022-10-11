feef1027dc50d00c6381e5121619ab94b0216d93
madhaviboga <113083121+madhaviboga@users.noreply.github.com>
2022-10-11 06:05:10 +0530

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,18 @@
+public class ForEach {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+				 int sum = 0;
+				 // use for-each style for to display and sum the values
+				 for(int x : nums) {
+				 System.out.println("Value is: " + x);
+				 sum += x;
+				 }
+				 System.out.println("Summation: " + sum);
+				
+
+			}
+
+		}
+		
