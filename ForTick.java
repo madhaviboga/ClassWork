36ce623bf24c24636dc838b529fb0af64d168ef1
madhaviboga <113083121+madhaviboga@users.noreply.github.com>
2022-10-11 06:06:00 +0530

    Create ForTick.java

---
+++ ForTick.java
@@ -0,0 +1,12 @@
+public class ForTick {
+
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int n;
+				for(n=10;n>0;n--)
+					System.out.println("tick"+n);
+				
+
+			}
+
+		}
