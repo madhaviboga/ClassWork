dae73f893ff14d50dbec75a4cb9166e4cf5b8a88
madhaviboga <113083121+madhaviboga@users.noreply.github.com>
2022-10-11 05:55:33 +0530

    Create Area.java

---
+++ Area.java
@@ -0,0 +1,11 @@
+area.java
+
+public class Area {
+	public static void main(String args[]) {
+		double r,pi,area;
+		r=10;
+		pi=3.1416;
+		area=pi*r*r;
+		System.out.println("Area is:"+area);
+		
+	}
